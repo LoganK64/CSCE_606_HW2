433eff458620a388cb82f356112dbdd74b937978
EC2 Default User <ec2-user@ip-172-31-13-208.us-east-2.compute.internal>
2021-02-24 22:20:45 +0000

    Commit 2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,6 +24,9 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
   end
 
+  def all_ratings
+    movie.all_ratings
+  end
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
