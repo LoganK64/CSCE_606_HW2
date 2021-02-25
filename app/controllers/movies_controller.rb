184bc991e44ce9ebd0619d3830dfdcd2e3321036
EC2 Default User <ec2-user@ip-172-31-13-208.us-east-2.compute.internal>
2021-02-25 21:48:15 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -55,10 +55,6 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-  def session_clear
-    reset_session
-  end
-  
   private
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
