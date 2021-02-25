18ca4c2f289d9726fd0d824ac42aade23d78ec75
EC2 Default User <ec2-user@ip-172-31-13-208.us-east-2.compute.internal>
2021-02-25 02:22:57 +0000

    Part 2 Final

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,17 +7,18 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all.order(params[:sort]) #set to all
+    @sort_mode = params[:sort]
+    @movies = Movie.all.order(@sort_mode) #set to all
     @all_ratings = Movie.all_ratings
     @ratings_to_show = Array.new
     if params[:ratings] != nil
       @ratings_to_show = params[:ratings].keys
-      @movies = Movie.with_ratings(params[:ratings].keys).order(params[:sort]) #set to just rating defined
+      @movies = Movie.with_ratings(params[:ratings].keys).order(@sort_mode) #set to just rating defined
     end
     if params[:ratings_2] != nil
       ratings_2 = Hash[params[:ratings_2].map {|x| [x,1]}]
       @ratings_to_show = ratings_2.keys
-      @movies = Movie.with_ratings(ratings_2.keys).order(params[:sort]) #set to just rating defined
+      @movies = Movie.with_ratings(ratings_2.keys).order(@sort_mode) #set to just rating defined
     end
   end
 
