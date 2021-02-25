b69536d324a746d95cb67b91bf7d98a4be208927
EC2 Default User <ec2-user@ip-172-31-13-208.us-east-2.compute.internal>
2021-02-25 21:34:12 +0000

    P3 Testing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'movies#index'
-  match 'clearSession' => 'movies#session_clear', :as => :session_clear
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
