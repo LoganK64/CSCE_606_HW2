f836cdba87c3645288eeb9d02ae138d7831648ee
EC2 Default User <ec2-user@ip-172-31-13-208.us-east-2.compute.internal>
2021-02-25 01:44:43 +0000

    Part 2 Final

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,13 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings
     @ratings_to_show = Array.new
     if params[:ratings] != nil
-      @ratings_to_show = params[:ratings].keys.map { |key| key.tr('\"','') }
-      @movies = Movie.with_ratings(params[:ratings].keys.map { |key| key.tr('\"','') }).order(params[:sort]) #set to just rating defined
+      @ratings_to_show = params[:ratings].keys
+      @movies = Movie.with_ratings(params[:ratings].keys).order(params[:sort]) #set to just rating defined
+    end
+    if params[:ratings_2] != nil
+      ratings_2 = Hash[params[:ratings_2].map {|x| [x,1]}]
+      @ratings_to_show = ratings_2.keys
+      @movies = Movie.with_ratings(ratings_2.keys).order(params[:sort]) #set to just rating defined
     end
   end
 
